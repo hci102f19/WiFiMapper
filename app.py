2e1aea1e9a2272613b107fbc9857b55200f0d58d
Kasper Helsted <khelst13@student.aau.dk>
2019-05-02 10:22:23 +0200

    Keep scanning

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import json
+import os
+from time import sleep
 
 from scanner import Scanner
 
 scanner = Scanner()
-spots = [x for x in scanner.scan()]
-sorted_spots = sorted(spots, key=lambda x: x['signal'], reverse=True)
 
-print(json.dumps(sorted_spots,indent=4))
+while True:
+    spots = [x for x in scanner.scan()]
+    sorted_spots = sorted(spots, key=lambda x: x['signal'], reverse=True)
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(json.dumps(sorted_spots[0], indent=4))
+
+    sleep(1)
