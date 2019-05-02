8036d6795f95ddb1f3a7ec8a722b8f24971e6d0d
Kasper Helsted <khelst13@student.aau.dk>
2019-05-02 10:19:33 +0200

    Pretty print

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import json
+
 from scanner import Scanner
 
 scanner = Scanner()
 spots = [x for x in scanner.scan()]
 sorted_spots = sorted(spots, key=lambda x: x['signal'], reverse=True)
 
-print(sorted_spots)
+print(json.dumps(sorted_spots,indent=4))
