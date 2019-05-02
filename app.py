9c39b5de4102525a83e27f3e1d74e35f85dde4fb
Kasper Helsted <khelst13@student.aau.dk>
2019-05-02 10:23:24 +0200

    Filter AP

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from scanner import Scanner
 scanner = Scanner()
 
 while True:
-    spots = [x for x in scanner.scan()]
+    spots = [x for x in scanner.scan() if x['essid'] == "AAU-1x"]
     sorted_spots = sorted(spots, key=lambda x: x['signal'], reverse=True)
 
     os.system('cls' if os.name == 'nt' else 'clear')
