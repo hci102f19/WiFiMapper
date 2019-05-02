73af5178d78bf37797cb4048fc9d9a167adf4ea0
Kasper Helsted <khelst13@student.aau.dk>
2019-05-02 10:19:02 +0200

    Sort spots

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from scanner import Scanner
 
 scanner = Scanner()
-print(scanner.scan())
+spots = [x for x in scanner.scan()]
+sorted_spots = sorted(spots, key=lambda x: x['signal'], reverse=True)
+
+print(sorted_spots)
